1cda80ffdcd206f6066a86fe3494c73bf10866c8
reebujoy <reebsmj@gmail.com>
2024-07-24 12:33:29 +0100

    Adds a simple Tic-Tac-Toe game

--- index.js
+++ index.js
@@ -1,43 +1,83 @@
-const btnA1 = document.getElementById('A1')
-const btnA2 = document.getElementById('A2')
-const btnA3 = document.getElementById('A3')
-const btnB1 = document.getElementById('B1')
-const btnB2 = document.getElementById('B2')
-const btnB3 = document.getElementById('B3')
-const btnC1 = document.getElementById('C1')
-const btnC2 = document.getElementById('C2')
-const btnC3 = document.getElementById('C3')
+const cells = document.querySelectorAll('.cell')
+const statusText = document.getElementById('statusText')
+const restartBtn = document.getElementById('btnRestart')
 
-const inputSection = document.querySelector('#InputSection p')
-let currentPlayer = 'X'
+const winconditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
+let options = [ "", "", "", "", "", "", "", "", ""]
+let currentPlayer = "X"
+let running = false;
 
-btnA1.addEventListener('click', (e)=>{
-    e.preventDefault()
-    addIcon(currentPlayer)
-})
-btnA2.addEventListener('click', (e)=>{
-    e.preventDefault()
-    addIcon(currentPlayer)
-})
-function addIcon(currentPlayer)
-{
-    const icon= document.createElement("img")
-    icon.classList.add("tic")
-    if(currentPlayer === 'X'){
-        icon.setAttribute("src", `images/x.png`)
-        icon.setAttribute("alt", "x")
-        inputSection.appendChild(icon)
-        console.log(currentPlayer)
-        changePlayer(currentPlayer)
+startGame();
+
+function startGame(){
+    cells.forEach(cell => cell.addEventListener("click",cellClicked))
+    restartBtn.addEventListener("click", restartGame)
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex")
+    if (options[cellIndex] != "" || !running){
+        return;
+    }
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer === 'X')? '0': 'X';
+    statusText.textContent = `${currentPlayer} 's turn`;
+}
+function checkWinner(){
+    let roundWon = false
+    for( let i=0; i<winconditions.length; i++) {
+        const condition = winconditions[i];
+        const cellA = options[condition[0]]
+        const cellB = options[condition[1]]
+        const cellC = options[condition[2]]
+
+        if(cellA == "" || cellB == "" || cellC ==""){
+            continue;
+        }
+        else if (cellA == cellB && cellB== cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
     }
     else{
-        icon.setAttribute("src", `images/o.png`)
-        icon.setAttribute("alt", "o")
-        inputSection.appendChild(icon)
-        changePlayer(currentPlayer)
+        changePlayer();
     }
 }
-function changePlayer(currentPlayer){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X" 
-    console.log(currentPlayer)
+function restartGame(){
+    currentPlayer = 'X'
+    options = [ "", "", "", "", "", "", "", "", ""]
+    statusText.textContent = `${currentPlayer}'s turn`
+    cells.forEach(cell => cell.textContent = "")
+    // running = true;
 }
+
+
+
+
+
+
